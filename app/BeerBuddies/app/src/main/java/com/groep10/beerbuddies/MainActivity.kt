3626279ff66eb057636c26c5e5204d6989164a85
Mullen,Pieter P.A.J. van der <github@vdmullen.nl>
2022-03-10 13:26:07 +0100

    Update MainActivity.kt

--- app/BeerBuddies/app/src/main/java/com/groep10/beerbuddies/MainActivity.kt
+++ app/BeerBuddies/app/src/main/java/com/groep10/beerbuddies/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.groep10.beerbuddies
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            var intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
