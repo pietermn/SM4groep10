6db5ca86708e273f5723cddadeb1771c710bd514
Pieter van Nunen <pietervannunen@hotmail.nl>
2022-04-12 10:48:35 +0200

    Start Custom Navigation

--- Android/MemoriesPOC/app/src/main/java/com/example/memoriespoc/MainActivity.kt
+++ Android/MemoriesPOC/app/src/main/java/com/example/memoriespoc/MainActivity.kt
@@ -1,23 +1,23 @@
 package com.example.memoriespoc
 
-import android.content.ClipData
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.memoriespoc.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val photo: FloatingActionButton = findViewById(R.id.floatingActionButton)
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
@@ -29,5 +29,11 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+       photo.setOnClickListener {
+           navController.navigate(R.id.photo)
+           Toast.makeText(applicationContext,"this is toast message",Toast.LENGTH_SHORT).show()
+
+       }
     }
 }
