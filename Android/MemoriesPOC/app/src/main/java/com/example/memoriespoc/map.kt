0d0c7b88b53ebc830f11e81e11b2707bc51975a2
Pieter van Nunen <pietervannunen@hotmail.nl>
2022-04-29 15:02:14 +0200

    Add Markers
    Markers added to mapview

--- Android/MemoriesPOC/app/src/main/java/com/example/memoriespoc/map.kt
+++ Android/MemoriesPOC/app/src/main/java/com/example/memoriespoc/map.kt
@@ -1,13 +1,19 @@
 package com.example.memoriespoc
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.memoriespoc.util.getData
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.fragment_map.*
 import kotlinx.android.synthetic.main.fragment_map.view.*
 
@@ -31,12 +37,19 @@ class map : Fragment(), OnMapReadyCallback {
 
         view.map_view.getMapAsync(this)
         // Inflate the layout for this fragment
+
+
+
         return view
     }
 
+
+
     override fun onMapReady(Map: GoogleMap) {
-        Map?.let {
-            googleMap = it
+
+        getData().forEach{
+            Map.addMarker(MarkerOptions().position(LatLng(it.latitude ,it.longitude)).title(it.city))
+
         }
     }
 
