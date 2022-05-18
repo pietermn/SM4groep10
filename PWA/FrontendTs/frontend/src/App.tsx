474cf6b9fc55d6ce5ad2486746c0af91c3ce0bfa
GivanWiggers <436715@student.fontys.nl>
2022-05-18 14:39:28 +0200

    CarCards with multiple cars already

--- PWA/FrontendTs/frontend/src/App.tsx
+++ PWA/FrontendTs/frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 import "./App.css";
+import CarCard from "./components/CarCard";
 
 const App = () => {
-  return <div className="main-container"></div>;
+  return <div className="main-container">
+    <div className="cars-container">
+      <CarCard name='Cooper SE' range='125' maxRange='450' status='Currently reserved' type='images/minicooperside' colour='Orange'></CarCard>
+      <CarCard name='BMW M4' range='125' maxRange='750' status='Currently avaiable' type='images/bmw2022sideview' colour='Gray'></CarCard>
+      <CarCard name='Cooper SE' range='125' maxRange='450' status='Currently reserved' type='images/minicooperside' colour='Orange'></CarCard>
+      <CarCard name='Porsche Taycan' range='125' maxRange='550' status='Currently reserved' type='images/porschetaycan' colour='White'></CarCard>
+      <CarCard name='BMW M4' range='125' maxRange='750' status='Currently avaiable' type='images/bmw2022sideview' colour='Gray'></CarCard>
+      <CarCard name='Porsche Taycan' range='125' maxRange='550' status='Currently reserved' type='images/porschetaycan' colour='White'></CarCard>
+    </div>
+  </div>;
 };
 
 export default App;
