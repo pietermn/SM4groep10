ea339f8c6ae91c94e8691aa7050b8c3f718c8f3a
Pieter van Nunen <pietervannunen@hotmail.nl>
2022-05-18 14:39:48 +0200

    Created AddCar and AddTransaction
    Created AddCar and AddTransaction

--- PWA/FrontendTs/frontend/src/App.tsx
+++ PWA/FrontendTs/frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import CarCard from "./components/CarCard";
+import TransactionCard from "./components/TransactionCard";
 
 import "./App.css";
+import AddCarCard from "./components/AddCarCard";
 
 const App = () => {
-  return <div className="main-container"></div>;
+  return <div className="main-container">
+    <TransactionCard></TransactionCard>
+    <AddCarCard></AddCarCard>
+  </div>;
 };
 
 export default App;
---
+++ PWA/FrontendTs/frontend/src/components/AddCarCard.tsx
@@ -0,0 +1,15 @@
+import {} from "@mui/material"
+import React from "react"
+import "./AddCarCard.scss"
+
+
+export default function AddCarCard(){
+
+
+    return(
+        <div className="Add-container">
+               <h2>Add new car</h2>
+               <img src="images/AddCar.png"></img>
+        </div>
+    );
+}
