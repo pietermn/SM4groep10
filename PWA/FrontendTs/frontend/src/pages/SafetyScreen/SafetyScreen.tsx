515b390f00b813c054eedc094ce07a9a494d8a2b
GivanWiggers <436715@student.fontys.nl>
2022-05-20 14:36:05 +0200

    adjustments safetyscreen

--- PWA/FrontendTs/frontend/src/pages/SafetyScreen/SafetyScreen.tsx
+++ PWA/FrontendTs/frontend/src/pages/SafetyScreen/SafetyScreen.tsx
@@ -6,13 +6,11 @@ import "./SafetyScreen.scss";
 const SafetyScreen = () => {
   const navigate = useNavigate();
   return <div className="main-container">
-    <div className="safety-container">
-      
-      <div className="roadBricks"></div>
+      <div className="roadBricks">
         <div className="roadBrick1" />
         <div className="roadBrick2" />
         <div className="roadBrick3" />
-      </div>
+        </div>
       <div className="Safety-container">
         <div>
           <h1 className="safetyNote">SAFETY NOTE</h1>
