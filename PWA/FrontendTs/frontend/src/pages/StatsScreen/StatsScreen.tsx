6f16af9dca77c045f3d8b1dafc41d9f724923fef
Mullen,Pieter P.A.J. van der <github@vdmullen.nl>
2022-05-23 10:28:02 +0200

    Update StatsScreen.tsx

--- PWA/FrontendTs/frontend/src/pages/StatsScreen/StatsScreen.tsx
+++ PWA/FrontendTs/frontend/src/pages/StatsScreen/StatsScreen.tsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
-import "./StatsScreen.scss"
+import "./StatsScreen.scss";
 import CarStatsCard from "../../components/CarStatsCard/CarStatsCard";
 import UserStatsCard from "../../components/UserStatsCard/UserStatsCard";
-import CarPageHeader from "../../components/CarPageHeader";
+import CarPageHeader from "../../components/CarPageHeader/CarPageHeader";
 import { CarTypeEnum } from "../../globaltypes";
 
 const StatsScreen = () => {
-
-  return <div className="main-container">
-      <div className="stats-containers">
-        <CarPageHeader name={"COOPER SE"} type={CarTypeEnum.minicooperside} colour={"Orange"} tripStatus={false}></CarPageHeader>
-        <CarStatsCard range={300} volume={30} odometer={11.304} consumption="1L:10km"></CarStatsCard>
-        
+    return (
+        <div className="main-container">
+            <div className="stats-containers">
+                <CarPageHeader
+                    name={"COOPER SE"}
+                    type={CarTypeEnum.minicooperside}
+                    colour={"Orange"}
+                    tripStatus={false}
+                ></CarPageHeader>
+                <CarStatsCard range={300} volume={30} odometer={11.304} consumption="1L:10km"></CarStatsCard>
+            </div>
         </div>
-  </div>;
+    );
 };
 
 export default StatsScreen;
