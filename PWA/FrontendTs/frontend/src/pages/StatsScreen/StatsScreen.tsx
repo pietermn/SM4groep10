06c8fcfd833fda1b47baf8a7d6cebf71b0b3e107
Pieter van Nunen <pietervannunen@hotmail.nl>
2022-05-30 13:50:20 +0200

    Added TripCard
    Added TripCard

--- PWA/FrontendTs/frontend/src/pages/StatsScreen/StatsScreen.tsx
+++ PWA/FrontendTs/frontend/src/pages/StatsScreen/StatsScreen.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./StatsScreen.scss";
 import CarStatsCard from "../../components/CarStatsCard/CarStatsCard";
 import UserStatsCard from "../../components/UserStatsCard/UserStatsCard";
+import TripCard from "../../components/TripCard/TripCard";
 import CarPageHeader from "../../components/CarPageHeader/CarPageHeader";
 import TransactionCard from "../../components/TransactionCard/TransactionCard";
 import { Car } from "../../globaltypes";
@@ -36,6 +37,11 @@ const StatsScreen = () => {
                     <TransactionCard transactions={car.transactions} />
                     <ChartComponent car={car} />
                     <div style={{ width: "100vw", height: "10px" }} />
+                    <div>
+                    {car.trips.map((Trips, i) => {
+                        return <TripCard Trip={Trips} key={i} />;
+                    })}
+                </div>
                 </div>
             )}
         </div>
