d8cb2e59c7d2377614eeb36771ec70efd2f7ef5b
Mullen,Pieter P.A.J. van der <github@vdmullen.nl>
2022-05-30 14:36:37 +0200

    Update Query.cs

--- PWA/Backend/pwaApi/Schema/Query.cs
+++ PWA/Backend/pwaApi/Schema/Query.cs
@@ -178,6 +178,17 @@ namespace pwaApi.Schema
                             User = Wim,
                             StartDate = DateTime.Parse("May 27, 2022, 14:00:00"),
                             EndDate = DateTime.Parse("May 27, 2022, 16:00:00")
+                        },new ReservationType{
+                            Id = 5,
+                            User = Kara,
+                            StartDate = DateTime.Parse("May 31, 2022, 09:00:00"),
+                            EndDate = DateTime.Parse("May 31, 2022, 12:00:00")
+                        },
+                        new ReservationType{
+                            Id = 6,
+                            User = Bob,
+                            StartDate = DateTime.Parse("May 31, 2022, 13:00:00"),
+                            EndDate = DateTime.Parse("May 31, 2022, 17:00:00")
                         }
                     },
                     Owner = Kara,
@@ -385,6 +396,17 @@ namespace pwaApi.Schema
                             User = Wim,
                             StartDate = DateTime.Parse("May 27, 2022, 14:00:00"),
                             EndDate = DateTime.Parse("May 27, 2022, 16:00:00")
+                        },new ReservationType{
+                            Id = 5,
+                            User = Kara,
+                            StartDate = DateTime.Parse("May 31, 2022, 09:00:00"),
+                            EndDate = DateTime.Parse("May 31, 2022, 12:00:00")
+                        },
+                        new ReservationType{
+                            Id = 6,
+                            User = Bob,
+                            StartDate = DateTime.Parse("May 31, 2022, 13:00:00"),
+                            EndDate = DateTime.Parse("May 31, 2022, 17:00:00")
                         }
                     },
                     Owner = Kara,
